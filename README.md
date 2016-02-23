37e0f4769577141fa9aca7df02b1360cb1a4ff6c
Brian Yarsawich <byarsawich@Brians-MBP.attlocal.net>
2016-02-23 01:48:20 -0500

    README update

--- README.md
+++ README.md
@@ -1,9 +1,31 @@
+#Weather Report
+
+##Description
+
+Use the weather undergound API to look up weather information based on postal code.
+
+This repository contains:
+
+* Class files for each end point of the weather underground API in use.
+* Gemfile for all required gems.
+* Test file
+
+##Components
+
+API end point classes:
+* Condition
+* Alert
+* Forecast
+* Astronomy
+* Hurricane
+
+##Estimate for Each Section
 estimates in hours
-normal:3
-hard:
-nightmare:
+* normal:3
+* hard: 1
+* nightmare:
 
 Work in hours
-normal:
-hard:
-nightmare:
+* normal: 4.5
+* hard:
+* nightmare:
--- weather_report.rb
+++ weather_report.rb
@@ -4,5 +4,20 @@ require './astronomy.rb'
 require './alert.rb'
 require './hurricane.rb'
 
-# puts "Enter a zip code:"
-# zip_code = gets.chomp
+puts "Enter a zip code:"
+zip_code = gets.chomp
+
+condition = Condition.new(zip_code)
+forcast = Forecase.new(zip_code)
+astronomy = Astronomy.new(zip_code)
+alert = Alert.new(zip_code)
+hurricane = Hurricane.new(zip_code)
+puts "Your current temperature in #{condition.location} is #{condition.temperature}"
+puts "Your ten day forcast is:"
+puts forcast.get_forecast
+puts "The sunrise will be at #{astronomy.sunrise}"
+puts "The sunset will be at #{astronomy.sunset}"
+puts "The current weather alerts:"
+puts alert.alerts
+puts "The current hurricanes are:"
+puts hurricne.get_hurricanes
